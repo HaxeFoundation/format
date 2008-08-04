1f1f57afc418828df76bffbe59b418edbe4e1e0b
Nicolas Cannasse <ncannasse@gmail.com>
2008-08-04 20:17:17 +0000

    no .svn in release

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@
 rm -rf release release.zip
 mkdir release
 cp -R format haxelib.xml release
-rm -rf release/.svn release/*/.svn
+rm -rf release/*/.svn release/*/*/.svn
 7z a -tzip release.zip release
 rm -rf release
 haxelib submit release.zip
