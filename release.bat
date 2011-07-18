c8765ea9955bc50364134b160c9de4ae4ec5646a
Nicolas Cannasse <ncannasse@gmail.com>
2011-07-18 10:18:21 +0000

    minor

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 @echo off
 rm -rf release release.zip
 mkdir release
-haxe -xml haxedoc.xml -cp tests/all All
+haxe -xml haxedoc.xml --macro include('format',true,['format.tools.MemoryInput','format.hxsl.Shader'])
 cp -R format haxelib.xml haxedoc.xml CHANGES.txt release
 rm -rf release/*/.svn release/*/*/.svn
 7z a -tzip release.zip release
