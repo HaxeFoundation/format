48cbb7bf7f951773de8d07edb72bc8d1e02124a3
Nicolas Cannasse <ncannasse@gmail.com>
2013-01-31 08:45:32 +0000

    fixed issue #50

--- format/swf/Writer.hx
+++ format/swf/Writer.hx
@@ -250,7 +250,7 @@ class Writer {
 			writeFixed8(d.strength);
 			writeFilterFlags(d.flags,true);
 		case FGradientGlow(d):
-			o.writeByte(5);
+			o.writeByte(4);
 			writeFilterGradient(d);
 		case FColorMatrix(d):
 			o.writeByte(6);
