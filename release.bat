f0062439e4278258aefe4cfd0df787d9b7b7c3e0
Nicolas Cannasse <ncannasse@gmail.com>
2013-02-01 17:06:04 +0000

    allow Win7 release

--- release.bat
+++ release.bat
@@ -1,9 +1,36 @@
 @echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
 rm -rf release release.zip
 mkdir release
 haxe -xml haxedoc.xml --macro include('format',true,['format.tools.MemoryInput','format.tools.MemoryBytes','format.hxsl.Shader'])
 cp -R format haxelib.xml haxedoc.xml CHANGES.txt release
 rm -rf release/*/.svn release/*/*/.svn
+chmod -R 777 release
 7z a -tzip release.zip release
 rm -rf release
 haxelib submit release.zip
