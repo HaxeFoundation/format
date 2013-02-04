7382eafe33fc168e6ab65bad71cf5075a4b61be0
Nicolas Cannasse <ncannasse@gmail.com>
2013-02-04 08:38:39 +0000

    fixed issue #53

--- format/swf/Writer.hx
+++ format/swf/Writer.hx
@@ -472,7 +472,7 @@ class Writer {
 				writeGradient(ver, grad);
 
 			case FSFocalGradient(mat, grad):
-				if(ver > 3)
+				if(ver < 3)
 					throw "Focal gradient fill style only supported with Shape versions higher than 3!";
 
 				o.writeByte(FillStyleTypeId.FocalRadialGradient);
