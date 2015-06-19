62480c64f72b4360b42ab950ec0495ad77900e60
Yanrishatum <krabanek@gmail.com>
2015-06-19 07:27:44 +0300

    Updated documentation and Tools. Incompatible with older Tools version.

--- format/gif/Data.hx
+++ format/gif/Data.hx
@@ -3,34 +3,54 @@ package format.gif;
 import haxe.io.Bytes;
 
 /**
- * ...
- * @author Yanrishatum
+ * Gif data.
  */
-
 typedef Data =
 {
+  /**
+   * Gif version. There is only 2 Gif version exists. 87a and 89a.
+   * 87a have less features and does not support any extensions.
+   * Unknown version is adviced to be interpreted as newest (89a) official version.
+   */
   var version:Version;
+  /**
+   * Information about logical screen of Gif that provides basic information about Gif.
+   */
   var logicalScreenDescriptor:LogicalScreenDescriptor;
+  /**
+   * Global color table used for Gif. Present only if Logical Screen Descriptor contained global color table flag.
+   * Note that this color table not always present since frames can contain local color tables that overrides global color table.
+   */
   @:optional var globalColorTable:Null<ColorTable>;
+  /**
+   * List of Gif data blocks.
+   */
   var blocks:List<Block>;
 }
 
+/**
+ * Gif data block. Custom blocks are not supported.
+ */
 enum Block
 {
   /**
    * Gif frame block.
+   * Note that this block does not contain link to graphic control extension of Frame even if it is present. GraphicControl extension Block commonly present right before frame Block.
    */
   BFrame(frame:Frame);
   /**
-   * Additional extension block.
+   * Additional extension block. This Block does not supported in 87a Gif specification version.
    */
   BExtension(extension:Extension);
   /**
-   * End of File block.
+   * End of File block. Represents end of Gif data.
    */
   BEOF;
 }
 
+/**
+ * Extension block contains additional data about Gif image. This block does not supported by 87a version.
+ */
 enum Extension
 {
   /**
@@ -46,7 +66,7 @@ enum Extension
    */
   EText(pte:PlainTextExtension);
   /**
-   * Application extension allow to insert additional application data into Gif. Most used app extension is NETSCAPE2.0 looping extension, used to set up amount of loops in frame.
+   * Application extension allow to insert additional application data into Gif. Mostly used app extension is NETSCAPE2.0 looping extension, used to set up amount of loops in frame.
    */
   EApplicationExtension(ext:ApplicationExtension);
   
@@ -56,9 +76,19 @@ enum Extension
   EUnknown(id:Int, data:Bytes);
 }
 
+/**
+ * Application extension. Mostly used only for one reason - setting up loops count. There is exist other app extensions but they are really rare.
+ */
 enum ApplicationExtension
 {
+  /**
+   * NETSCAPE2.0 looping extension. Contains only amount of animation repeats.
+   * Note that there is two NETSCAPE2.0 app extensions for Gif format and the type of extension is stored in first byte of data. Looping extension have ID 1.
+   */
   AENetscapeLooping(loops:Int);
+  /**
+   * Unknown or unsupported app extension.
+   */
   AEUnknown(name:String, version:String, data:Bytes);
 }
 
@@ -69,6 +99,16 @@ enum ApplicationExtension
  */
 typedef ColorTable = Bytes;
 
+/**
+ * Single frame of the image.
+ * Actually it's a merge of 3 consequent blocks:
+ * 1. Image Descriptor.
+ * Contains frame informations like position, size, existing of local color table and interlaced flag.
+ * 2. [Local color table].
+ * Only present if Image Descriptor contains local color table flag. Overrides global color table.
+ * 3. Pixel data blocks.
+ * LZW compressed pixel data.
+ */
 typedef Frame =
 {
   /**
@@ -98,6 +138,7 @@ typedef Frame =
   
   /**
    * Is this image written in interlace mode?
+   * Note: The pixel data already deinterlaced and this flag presented only for information purpose (and for Writer when there is one).
    */
   var interlaced:Bool;
   
@@ -133,7 +174,7 @@ typedef GraphicControlExtension =
   var disposalMethod:DisposalMethod;
   /**
    * Is image must wait for user input, before dispose?
-   * This flag may be used by user-defined program.
+   * This flag may be used by user-defined program but absolutely ignored by any Gif players.
    */
   var userInput:Bool;
   /**
@@ -142,6 +183,8 @@ typedef GraphicControlExtension =
   var hasTransparentColor:Bool;
   /**
    * Delay, before next image appears. Delay is in centiseconds (1 centisecond = 1/100 seconds).
+   * Note: Some players (like FastStone) cut fraction of elapsed time when progressing to next frame which results in small timing error.
+   * Recommended to use `time -= delay` instead of `time = 0`.
    */
   var delay:Int;
   /**
@@ -150,21 +193,55 @@ typedef GraphicControlExtension =
   var transparentIndex:Int;
 }
 
+/**
+ * Extension for rendering text on Gif logical screen. It does not supported by major Gif decoders.
+ * Font and text size decision is left to decoder. (recommended to decide based on grid/cell size)
+ * Text must be rendered with one character at cell.
+ * It's recommended to replace any characters less than 0x20 and greater than 0xf7 to be rendered as Space (0x20)
+ */
 typedef PlainTextExtension =
 {
+  /**
+   * X position of text grid on Logical Screen.
+   */
   var textGridX:Int;
+  /**
+   * Y position of text grid on Logical Screen.
+   */
   var textGridY:Int;
+  /**
+   * Width of text grid in pixels.
+   */
   var textGridWidth:Int;
+  /**
+   * Height of text grid in pixels.
+   */
   var textGridHeight:Int;
+  /**
+   * Width of character cell in text grid.
+   */
   var charCellWidth:Int;
+  /**
+   * Height of character cell in text grid.
+   */
   var charCellHeight:Int;
+  /**
+   * Foreground/character color index.
+   */
   var textForegroundColorIndex:Int;
+  /**
+   * Background color index.
+   */
   var textBackgroundColorIndex:Int;
+  /**
+   * Text to render.
+   */
   var text:String;
 }
 
 /**
  * Logical screen descriptor of GIF file.
+ * Contains very basic information about Gif.
  */
 typedef LogicalScreenDescriptor =
 {
@@ -232,13 +309,13 @@ typedef LogicalScreenDescriptor =
 enum Version
 {
   /**
-   * First version of Gif file format from year 1987.
+   * First version of Gif file format from May 1987.
    * 
    * Note: The checking of unsupported blocks disabled by default to save some time. To enable supported blocks check set `yagp_strict_version_check` debug variable.
    */
   GIF87a;
   /**
-   * Second and actual version of Gif file format from year 1989.
+   * Second and actual version of Gif file format from July 1989.
    */
   GIF89a;
   /**
--- format/gif/Tools.hx
+++ format/gif/Tools.hx
@@ -5,12 +5,14 @@ import haxe.io.Bytes;
 import haxe.io.BytesData;
 
 /**
- * ...
+ * Tools for gif data.
  * @author Yanrishatum
  */
 class Tools
 {
-
+  /**
+   * Returns amount of frames in Gif data.
+   */
   public static function framesCount(data:Data):Int
   {
     var frames:Int = 0;
@@ -26,34 +28,46 @@ class Tools
     return frames;
   }
   
-  public static function frameAtIndex(data:Data, frameIndex:Int):Frame
+  /**
+   * Returns frame at given index.
+   * @param data Gif data.
+   * @param frameIndex Index of frame.
+   * @return Frame at given index or null, if there is no frame at that index.
+   */
+  public static function frame(data:Data, frameIndex:Int):Frame
   {
-    var frames:Int = 0;
+    var counter:Int = 0;
     for (block in data.blocks)
     {
-      switch(block)
+      switch (block)
       {
         case Block.BFrame(frame):
-          if (frames == frameIndex) return frame;
-          frames++;
+          if (counter == frameIndex) return frame;
+          counter++;
         default :
       }
     }
     return null;
   }
   
-  public static function frameGraphicControlExtension(data:Data, frameIndex:Int):GraphicControlExtension
+  /**
+   * Returns Graphic Control extension for frame at given index.
+   * @param data Gif data.
+   * @param frameIndex Index of frame.
+   * @return GCE extension if it is exists for given frame, null otherwise.
+   */
+  public static function graphicControl(data:Data, frameIndex:Int):GraphicControlExtension
   {
-    var frames:Int = 0;
+    var counter:Int = 0;
     var gce:GraphicControlExtension = null;
     for (block in data.blocks)
     {
       switch (block)
       {
         case Block.BFrame(frame):
-          if (frames == frameIndex) return gce;
+          if (counter == frameIndex) return gce;
           gce = null;
-          frames++;
+          counter++;
         case Block.BExtension(Extension.EGraphicControl(g)):
           gce = g;
         default :
@@ -62,6 +76,17 @@ class Tools
     return null;
   }
   
+  //==========================================================
+  // Extracting.
+  //==========================================================
+  
+  /**
+   * Extracts frame pixel data in Blue-Green-Red-Alpha pixel format.
+   * This function extracts only exact frame and does put previous frame pixel data into resulting Bytes. Note that frame size may not equal to Gif logical screen size.
+   * @param data Gif data.
+   * @param frameIndex Frame index.
+   * @return BGRA pixel data with dimensions equals to specified Frame size. If frame does not present in Gif data returns null.
+   */
   public static function extractBGRA(data:Data, frameIndex:Int):Bytes
   {
     var gce:GraphicControlExtension = null;
@@ -106,6 +131,13 @@ class Tools
     return null;
   }
   
+  /**
+   * Extracts frame pixel data in Red-Green-Blue-Alpha pixel format.
+   * This function extracts only exact frame and does put previous frame pixel data into resulting Bytes. Note that frame size may not equal to Gif logical screen size.
+   * @param data Gif data.
+   * @param frameIndex Frame index.
+   * @return RGBA pixel data with dimensions equals to specified Frame size. If frame does not present in Gif data returns null.
+   */
   public static function extractRGBA(data:Data, frameIndex:Int):Bytes
   {
     var gce:GraphicControlExtension = null;
@@ -150,6 +182,13 @@ class Tools
     return null;
   }
   
+  /**
+   * Extracts full Gif pixel data to specified frame in Blue-Green-Red-Alpha pixel format.
+   * This functions returns full representation of frame including rendering of all other frames before.
+   * @param data Gif data.
+   * @param frameIndex Frame index.
+   * @return BGRA pixel data with dimensions equals to Gif logical screen with full pixel data of Gif image at specified frame.
+   */
   public static function extractFullBGRA(data:Data, frameIndex:Int):Bytes
   {
     var gce:GraphicControlExtension = null;
@@ -229,14 +268,100 @@ class Tools
     return bytes;
   }
   
-  public static function buildFrameFromTrueColor(pixels:Bytes, width:Int, height:Int):Void
+  /**
+   * Extracts full Gif pixel data to specified frame in Red-Green-Blue-Alpha pixel format.
+   * This functions returns full representation of frame including rendering of all other frames before.
+   * @param data Gif data.
+   * @param frameIndex Frame index.
+   * @return RGBA pixel data with dimensions equals to Gif logical screen with full pixel data of Gif image at specified frame.
+   */
+  public static function extractFullRGBA(data:Data, frameIndex:Int):Bytes
   {
+    var gce:GraphicControlExtension = null;
+    var frameCaret:Int = 0;
     
+    var bytes:Bytes = Bytes.alloc(data.logicalScreenDescriptor.width* data.logicalScreenDescriptor.height * 4);
+    
+    for (block in data.blocks)
+    {
+      switch (block)
+      {
+        case Block.BExtension(ext):
+          switch(ext)
+          {
+            case Extension.EGraphicControl(g):
+              gce = g;
+            default:
+          }
+        case Block.BFrame(frame):
+          var ct:Bytes = frame.localColorTable ? frame.colorTable : data.globalColorTable;
+          if (ct == null) throw "Frame does not have a color table!";
+          var transparentIndex:Int = gce != null && gce.hasTransparentColor ? gce.transparentIndex * 3 : -1;
+          var pixels:Bytes = frame.pixels;
+          var x:Int = 0;
+          var writeCaret:Int = (frame.y * data.logicalScreenDescriptor.width + frame.x) * 4;
+          var lineSkip:Int = (data.logicalScreenDescriptor.width - frame.width) * 4 + 4;
+          
+          var disposalMethod:DisposalMethod = frameCaret != frameIndex && gce != null ? gce.disposalMethod : DisposalMethod.NO_ACTION;
+          
+          switch (disposalMethod)
+          {
+            case DisposalMethod.RENDER_PREVIOUS:
+              // Do not render frame at all
+            case DisposalMethod.FILL_BACKGROUND:
+              for (i in 0...pixels.length)
+              {
+                bytes.set(writeCaret    , 0); // R
+                bytes.set(writeCaret + 1, 0); // G
+                bytes.set(writeCaret + 2, 0); // B
+                bytes.set(writeCaret + 3, 0); // A
+                
+                if (++x == frame.width)
+                {
+                  x = 0;
+                  writeCaret += lineSkip;
+                }
+                else writeCaret += 4;
+              }
+            default:
+              for (i in 0...pixels.length)
+              {
+                var index:Int = pixels.get(i) * 3;
+                if (transparentIndex != index) // Render only if pixel non-transparent
+                {
+                  bytes.set(writeCaret    , ct.get(index    )); // R
+                  bytes.set(writeCaret + 1, ct.get(index + 1)); // G
+                  bytes.set(writeCaret + 2, ct.get(index + 2)); // B
+                  bytes.set(writeCaret + 3, 0xFF);              // A
+                }
+                
+                if (++x == frame.width)
+                {
+                  x = 0;
+                  writeCaret += lineSkip;
+                }
+                else writeCaret += 4;
+              }
+          }
+          
+          if (frameCaret == frameIndex) return bytes;
+          frameCaret++;
+          gce = null;
+        default:
+      }
+    }
+    
+    return bytes;
   }
   
+  /**
+   * Returns amount of animation repeats stored in Gif data.
+   * This is link to Netscape Looping application extension. If this extension does not present amount of loops equals to 1.
+   * @param data Gif data.
+   * @return Amount of animation repeats. Zero equals to infinite amount of repeats.
+   */
   public static function loopCount(data:Data):Int
   {
-    var frames:Int = 0;
     for (block in data.blocks)
     {
       switch(block)
@@ -245,8 +370,17 @@ class Tools
         default :
       }
     }
-    return frames;
+    return 1;
   }
+  
+  //==========================================================
+  // In-Dev writer tools.
+  //==========================================================
+  
+  //public static function buildFrameFromTrueColor(pixels:Bytes, width:Int, height:Int):Void
+  //{
+    //
+  //}
   
   private static var LN2:Float = Math.log(2);
   @:noCompletion public static inline function log2(val:Float):Float
