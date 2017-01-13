d7531cb2c9959dee3b5d17c99d900463032f6c69
Nicolas Cannasse <ncannasse@gmail.com>
2017-01-13 01:25:02 +0100

    added hltype toString

--- format/hl/Tools.hx
+++ format/hl/Tools.hx
@@ -33,4 +33,51 @@ class Tools {
 		return h;
 	}
 
+	public static function toString( t : HLType ) {
+		if( t == null )
+			return "<null>";
+		inline function fstr(t) {
+			return switch(t) {
+			case HFun(_): "(" + toString(t) + ")";
+			default: toString(t);
+			}
+		};
+		return switch( t ) {
+		case HVoid: "Void";
+		case HUi8: "hl.UI8";
+		case HUi16: "hl.UI16";
+		case HI32: "Int";
+		case HF32: "Single";
+		case HF64: "Float";
+		case HBool: "Bool";
+		case HBytes: "hl.Bytes";
+		case HDyn: "Dynamic";
+		case HFun(f):
+			if( f.args.length == 0 ) "Void -> " + fstr(f.ret) else [for( a in f.args ) fstr(a)].join(" -> ") + " -> " + fstr(f.ret);
+		case HObj(o):
+			o.name;
+		case HArray:
+			"hl.NativeArray";
+		case HType:
+			"hl.Type";
+		case HRef(t):
+			"hl.Ref<" + toString(t) + ">";
+		case HVirtual(fl):
+			var fields = [for( f in fl ) { name : f.name, t : f.t }];
+			for( f in fl ) f.t = HAt(0); // mark for recursion
+			var str = "{ " + [for( f in fields ) f.name+" : " + toString(f.t)].join(", ") + " }";
+			for( i in 0...fields.length ) fl[i].t = fields[i].t;
+			str;
+		case HDynObj:
+			"hl.DynObj";
+		case HAbstract(name):
+			"hl.NativeAbstract<" + name+">";
+		case HEnum(e):
+			e.name;
+		case HNull(t):
+			"Null<" + toString(t) + ">";
+		case HAt(_):
+			"<...>";
+		}
+	}
 }
