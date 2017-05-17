ba1d2f3edad1b139a1b3a906693b0b5856ee35ed
Dan Korostelev <nadako@gmail.com>
2017-05-17 15:36:43 +0300

    sigh

--- doc-deploy.sh
+++ doc-deploy.sh
@@ -55,7 +55,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+chmod 600 ../../deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
