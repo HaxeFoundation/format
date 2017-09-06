13b3da8343d5f102db13bea26e7c830c37e1ac95
Hendrik Klindworth <hendrik.klindworth@innogames.de>
2017-09-06 09:17:42 +0200

    Write glyph advance as unsigned instead of signed Int16 (#64)

--- format/swf/Writer.hx
+++ format/swf/Writer.hx
@@ -1123,10 +1123,7 @@ class Writer {
 			o.writeInt16(data.layout.leading);
 
 			for (g in data.layout.glyphs) {
-				var advance = if(g.advance > 32767) 32767
-					else if(g.advance < -32768) -32768
-					else g.advance;
-				o.writeInt16(advance);
+				o.writeUInt16(if( g.advance < 0 ) 0 else if( g.advance > 0xFFFF ) 0xFFFF else g.advance);
 			}
 
 			for(g in data.layout.glyphs)
