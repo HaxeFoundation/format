HaxeFoundation/format

commit ac10f3f508611b9c1175fb198c9a0dab3201c39e

Amf3 class handling (#89)

format/amf3/Reader.hx
@@ -71,11 +71,11 @@ class Reader {
 		else if( n & 3 == 1 )
 		{
 			// object traits reference
-			n >>= 3;
+			n >>= 2;
 			var refTraits = objectTraitsTable[n];
 			dyn = refTraits.isDynamic;
 			isExternalizable = refTraits.isExternalizable;
-			//className = refTraits.className;
+			className = refTraits.className;
 			//trace(Tools.decode(className));  // TODO make registered class feature or use Type.resolveClass?
 			sealedMemberNames = refTraits.sealedMemberNames;
 		}
@@ -114,7 +114,7 @@ class Reader {
 
 		var h = new Map();
 
-		var ret = AObject( h );
+		var ret = AObject( h, className != null ? Tools.decode(className) : null );
 
 		// save new object in reference table
 		complexObjectsTable.push( ret );
@@ -210,7 +210,7 @@ class Reader {
 			a[r] = AInt( i.readInt32() );
 		}
 
-		var ret = fixed? AVector( a ) : AArray( a );
+		var ret = fixed? AVector( a, "Int" ) : AArray( a );
 
 		complexObjectsTable.push(ret);
 
@@ -238,7 +238,7 @@ class Reader {
 			a[r] = ANumber( i.readDouble() );
 		}
 
-		var ret = fixed? AVector( a ) : AArray( a );
+		var ret = fixed? AVector( a, "Number" ) : AArray( a );
 
 		complexObjectsTable.push(ret);
 
@@ -264,7 +264,7 @@ class Reader {
 		if( fixed )
 		{
 			a = new Vector(len);
-			ret = AVector( a );
+			ret = AVector( a, objectTypeName );
 		}
 		else
 		{

format/amf3/Tools.hx
@@ -42,7 +42,7 @@ class Tools {
 			for ( f in Reflect.fields(o) ) {
 				h.set(f, encode(Reflect.field(o, f)));
 			}
-			AObject(h);
+			AObject(h, null);
 		case TClass(c):
 			switch( c ) {
 			case cast String:
@@ -77,7 +77,7 @@ class Tools {
 				var a = new Vector<Value>(o.length);
 				for(i in 0...o.length)
 					a[i] = encode(o[i]);
-				AVector(a);
+				AVector(a, null);
 			#end
 			case cast haxe.io.Bytes:
 				ABytes(o);
@@ -86,11 +86,12 @@ class Tools {
 			case _:
 				var h = new Map();
 				var i = 0;
-				for ( f in Type.getInstanceFields(Type.getClass(o)) ) {
+				var _class = Type.getClass(o);
+				for ( f in Type.getInstanceFields(_class) ) {
 					h.set(f, encode(Reflect.getProperty(o, f)));
 					i++;
 				}
-				AObject(h, i);
+				AObject(h, Type.getClassName(_class), i);
 			}
 		default:
 			throw "Can't encode "+Std.string(o);
@@ -181,7 +182,7 @@ class Tools {
 	public static function vector( a : Value ) {
 		if( a == null ) return null;
 		return switch( a ) {
-			case AVector(a):
+			case AVector(a,_):
 				var v = new Vector<Dynamic>(a.length);
 				for (i in 0...a.length)
 					v[i] = decode(a[i]);

format/amf3/Value.hx
@@ -36,9 +36,9 @@ enum Value {
 	ANumber( f : Float );
 	AString( s : String );
 	ADate( d : Date );
-	AObject( fields : Map<String,Value>, ?size : Int );
+	AObject( fields : Map<String,Value>, classname: String, ?size : Int );
 	AArray( values : Array<Value>, ?extra : Map<String,Value> );
-	AVector( values : Vector<Value> );
+	AVector( values : Vector<Value>, classname: String );
 	AXml( x : Xml );
 	ABytes( b : haxe.io.Bytes );
 	AMap( m : Map<Value, Value> );

commit 891d28ffcdaf716721e6f65eeeb56a9f00f711a4

Fix amf3 issues caused by ac10f3f
The changes in Value.AObject from ac10f3f caused amf3.Writer to fail. This change makes classname optional (and last in order), to maintain compatibility with Writer and any external code using amf3.Value (eg. https://github.com/openfl/openfl/blob/1628cf58c1a0b2d556b31215d152641bb85fa379/packages/utils/src/openfl/utils/ByteArray.hx#L1388)

format/amf3/Reader.hx
@@ -114,7 +114,7 @@ class Reader {
 
 		var h = new Map();
 
-		var ret = AObject( h, className != null ? Tools.decode(className) : null );
+		var ret = AObject( h, null, className != null ? Tools.decode(className) : null );
 
 		// save new object in reference table
 		complexObjectsTable.push( ret );

format/amf3/Tools.hx
@@ -42,7 +42,7 @@ class Tools {
 			for ( f in Reflect.fields(o) ) {
 				h.set(f, encode(Reflect.field(o, f)));
 			}
-			AObject(h, null);
+			AObject(h, null, null);
 		case TClass(c):
 			switch( c ) {
 			case cast String:
@@ -91,7 +91,7 @@ class Tools {
 					h.set(f, encode(Reflect.getProperty(o, f)));
 					i++;
 				}
-				AObject(h, Type.getClassName(_class), i);
+				AObject(h, i, Type.getClassName(_class));
 			}
 		default:
 			throw "Can't encode "+Std.string(o);
@@ -109,7 +109,7 @@ class Tools {
 			case ADate(_): date(a);
 			case AArray(_,_): array(a);
 			case AVector(_): vector(a);
-			case AObject(_,_): object(a);
+			case AObject(_,_,_): object(a);
 			case AXml(_): xml(a);
 			case ABytes(_): bytes(a);
 			case AMap(_): map(a);
@@ -194,7 +194,7 @@ class Tools {
 	public static function object( a : Value ) {
 		if( a == null ) return null;
 		return switch( a ) {
-		case AObject(o, _):
+		case AObject(o, _, _):
 			var m = new Map();
 			for (f in o.keys())
 				m.set(f, decode(o.get(f)));

format/amf3/Value.hx
@@ -36,9 +36,9 @@ enum Value {
 	ANumber( f : Float );
 	AString( s : String );
 	ADate( d : Date );
-	AObject( fields : Map<String,Value>, classname: String, ?size : Int );
+	AObject( fields : Map<String,Value>, ?size : Int, ?classname: String );
 	AArray( values : Array<Value>, ?extra : Map<String,Value> );
-	AVector( values : Vector<Value>, classname: String );
+	AVector( values : Vector<Value>, ?classname: String );
 	AXml( x : Xml );
 	ABytes( b : haxe.io.Bytes );
 	AMap( m : Map<Value, Value> );